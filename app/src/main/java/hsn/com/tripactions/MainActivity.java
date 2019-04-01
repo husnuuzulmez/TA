a43dcf32c3e3d8de23ce7ed68ae1886f85fb8e2f
Husnu Uzulmez <h.uzulmez@criteo.com>
2019-04-01 16:00:23 -0700

    anew1

--- app/src/main/java/hsn/com/tripactions/MainActivity.java
+++ app/src/main/java/hsn/com/tripactions/MainActivity.java
@@ -52,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra("largethumbnail" ,story.getLargethumbnail() );
                 intent.putExtra("url" ,story.getShgort_url() );
                 //heytyttt
+                //ahanda ahanaaaa
                 startActivity(intent);
             }
 
